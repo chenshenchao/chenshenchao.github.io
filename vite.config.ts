e47963ce4bf51b97fb9b7417d31e459aecfdc648
chenshenchao <chenshenchao@outlook.com>
2023-10-10 00:34:13 +0800

    output.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import _ from 'lodash'; // CommonJs 不支持 import { trim } from 'lodash';
+
+// GitHub Pages 静态服务器不支持在路径里的符号（以下正则）和 _ 开头
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name: string) {
+          return _.trim(name, '_').replace(INVALID_CHAR_REGEX, "");
+        },
+      },
+    },
+  },
+});
