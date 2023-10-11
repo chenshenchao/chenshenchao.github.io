6fde8155be09fe114d85057c12bf83515679316c
chenshenchao <chenshenchao@outlook.com>
2023-10-12 01:17:10 +0800

    编译前端。

---
+++ assets/CenterPartPage-f682c9fb.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vueexport-helper-c27b6911.js";import{c as s,i as a,f as r,A as _}from"./index-0bc6031d.js";const c={},n={class:"center-part-page"},o={class:"center-part"};function d(e,f){return s(),a("div",n,[r("div",o,[_(e.$slots,"default",{},void 0,!0)])])}const p=t(c,[["render",d],["__scopeId","data-v-f4714e35"]]);export{p as default};
