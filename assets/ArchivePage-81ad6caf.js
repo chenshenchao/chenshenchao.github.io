f9c020d4b4830ed772f3d6b67320cbb54026fb44
chenshenchao <chenshenchao@outlook.com>
2024-08-25 01:35:58 +0800

    编译。

---
+++ assets/ArchivePage-81ad6caf.js
@@ -0,0 +1,29 @@
+import{b as v,o as f}from"./vue-router-0c49f893.js";import{l as g}from"./archive-e964beff.js";import{v as y,d as A,c as b,p as C,D as d,A as P,G as k,F as l,E as w,L as t,M as u,N as H,O as x}from"./@vue-9cc5f714.js";import{_ as B}from"./plugin-vueexport-helper-c27b6911.js";import"./highlight.js-23c08a4f.js";import"./crypto-js-4677913a.js";import"./highlight.js-lib-languages-1-b101391b.js";import"./highlight.js-lib-languages-a-16a177ab.js";import"./highlight.js-lib-languages-x-364ed794.js";import"./highlight.js-lib-languages-b-b6e567a1.js";import"./highlight.js-lib-languages-c-234f4f8e.js";import"./highlight.js-lib-languages-d-7b8159cb.js";import"./highlight.js-lib-languages-m-b6de83b3.js";import"./highlight.js-lib-languages-e-fad85485.js";import"./highlight.js-lib-languages-r-53dfddc0.js";import"./highlight.js-lib-languages-f-8820c8d5.js";import"./highlight.js-lib-languages-g-ea579cf1.js";import"./highlight.js-lib-languages-h-74f34ea6.js";import"./highlight.js-lib-languages-i-d0f5bb2d.js";import"./highlight.js-lib-languages-j-ef705224.js";import"./highlight.js-lib-languages-k-220e6614.js";import"./highlight.js-lib-languages-l-555830da.js";import"./highlight.js-lib-languages-p-f2205468.js";import"./highlight.js-lib-languages-n-816e5f3a.js";import"./highlight.js-lib-languages-o-b41baf90.js";import"./highlight.js-lib-languages-q-2501bc69.js";import"./highlight.js-lib-languages-s-a87df519.js";import"./highlight.js-lib-languages-t-23d3f6da.js";import"./highlight.js-lib-languages-y-3713b1da.js";import"./highlight.js-lib-languages-v-73a12446.js";import"./highlight.js-lib-languages-w-568a4b3d.js";import"./highlight.js-lib-languages-z-d809bd39.js";import"./marked-59892390.js";import"./marked-highlight-d2522e5a.js";const i=r=>(H("data-v-25ad2611"),r=r(),x(),r),G=i(()=>t("div",{class:"background"},null,-1)),I={class:"archive-info"},J={class:"archive-info-item"},S=i(()=>t("span",null,"创建日期：",-1)),V={class:"archive-info-item"},L=i(()=>t("span",null,"修改日期：",-1)),M=["innerHTML"],N=y({__name:"ArchivePage",setup(r){const _=v(),a=A({content:"",path:""}),s=b(()=>[{path:"bookshelf/the-programming-language-java.md",createAt:"2024-08-11",modifyAt:"2024-08-11",mtime:172338238e4,title:"Java 书籍\r",summary:`# Java 书籍\r
+\r
+以下是我阅读过的 Java 技术书籍的评价。\r
+\r
+---\r
+\r
+## 《Java 虚拟机字节码：从入门到实战》\r
+\r
+�`},{path:"bookshelf/the-programming-language-csharp.md",createAt:"2024-08-11",modifyAt:"2024-08-11",mtime:1723366814e3,title:"C# 书籍\r",summary:`# C# 书籍\r
+\r
+以下是我阅读过的 C# 技术书籍的评价。\r
+\r
+---\r
+\r
+## 《C# 并发编程经典实例》\r
+\r
+推荐指数：★★�`},{path:"reading/one-hundred-years-of-solitude.md",createAt:"2023-11-09",modifyAt:"2024-03-23",mtime:1711130162e3,title:"读《百年孤独》\r",summary:`# 读《百年孤独》\r
+\r
+人类的意义是什么？——不断创造意义。\r
+\r
+意义的载体是什么？——逻辑。\r
+\r
+人生不`},{path:"reading/don-quijote-de-la-mancha.md",createAt:"2023-11-24",modifyAt:"2023-11-24",mtime:1700755857e3,title:"读《堂吉诃德》\r",summary:`# 读《堂吉诃德》\r
+\r
+塞万提斯的人生经理一点不比堂吉诃德的故事逊色。如果用现在人的眼光去看待堂吉�`},{path:"reading/catch-22.md",createAt:"2023-10-27",modifyAt:"2023-10-27",mtime:169834954e4,title:"读《第22条军规》\r",summary:`# 读《第22条军规》\r
+\r
+本书的章节名很有意思，每章都是一个人名，最后以主角约瑟连做章名完结。章名可`},{path:"essays/github-pages-with-vue3.md",createAt:"2023-10-11",modifyAt:"2023-10-16",mtime:1697395601e3,title:"用 Vue 3 写 GitHub Pages\r",summary:`# 用 Vue 3 写 GitHub Pages\r
+\r
+要写博客的话 GitHub Pages 是个不错的选择。但是 GitHub Pages 是静态的，并不能像常�`}].find(e=>e.path==a.path)),m=async e=>{const o=e.params.path,n=`/archives/${o}`;o!=a.path&&(a.path=o,a.content=await g(n))};return C(async()=>{console.log("onActivied"),await m(_)}),f(async(e,o)=>{console.log("onBeforeRouteUpdate"),await m(e)}),(e,o)=>{const n=d("pc-banner"),h=d("pc-center-part-layout");return P(),k(h,{class:"pc-archive-page"},{background:l(()=>[G]),default:l(()=>{var p,c;return[w(n),t("div",I,[t("div",J,[S,t("span",null,u((p=s.value)==null?void 0:p.createAt),1)]),t("div",V,[L,t("span",null,u((c=s.value)==null?void 0:c.modifyAt),1)])]),t("div",{class:"archive-sheet",innerHTML:a.content},null,8,M)]}),_:1})}}});const ft=B(N,[["__scopeId","data-v-25ad2611"]]);export{ft as default};
