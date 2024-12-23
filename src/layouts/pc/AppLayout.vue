f7cb51dbb2e5c7122fe2ad519e4912ddf9034e09
chenshenchao <chenshenchao@outlook.com>
2024-12-24 01:34:44 +0800

    save.

--- src/layouts/pc/AppLayout.vue
+++ src/layouts/pc/AppLayout.vue
@@ -18,6 +18,7 @@ const loadingElement = ref<HTMLDivElement>();
 
 onMounted(() => {
     document.body.appendChild(loadingElement.value!);
+    // appStore.isLoading = true;
 });
 
 onBeforeUnmount(() => {
@@ -28,6 +29,8 @@ onBeforeUnmount(() => {
 <style lang="scss" scoped>
 .pc-loading {
     display: flex;
+    align-items: center;
+    justify-content: center;
     position: fixed;
     top: 0;
     left: 0;
@@ -35,5 +38,39 @@ onBeforeUnmount(() => {
     bottom: 0;
     z-index: 100000;
     background-color: #4444;
+
+    &::before {
+        content: "";
+        position: relative;
+        width: 6.4vw;
+        height: 6.4vw;
+        background-image: radial-gradient(closest-side, #fff, #ebf8e144, #f69d3c44, #fff4);
+        box-shadow: 0 0 .4vw .4vw #fff;
+        border-radius: 50%;
+
+        transform-origin: bottom left;
+
+        animation: 4s linear infinite running loading-rotate;
+    }
+}
+
+@keyframes loading-rotate {
+    0% {
+        background-position: 0 0;
+        transform: rotate(0deg);
+        box-shadow: 0 0 .4vw .4vw #fff;
+    }
+
+    50% {
+        background-position: 50% 100%;
+        transform: rotate(180deg);
+        box-shadow: 0 0 4vw 4vw #fff;
+    }
+
+    100% {
+        background-position: 0 0;
+        transform: rotate(360deg);
+        box-shadow: 0 0 .4vw .4vw #fff;
+    }
 }
 </style>
